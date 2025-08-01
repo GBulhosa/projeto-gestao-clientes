1f03560f188b064b3721c3b3116683023e087c9b
Gabriel Silva Bulhosa <GBulhosa@brasal.com.br>
2025-08-01 17:24:46 -0300

    Remover importação não usada do React

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Layout from './components/layout/Layout';
